21014f0812f1a07340adc11523af46a13fb705ae
LaurenRuuyx <laurmscc@gmail.com>
2025-04-04 13:29:12 +0100

    changes and fixes

--- PythonProject/request_test.py
+++ PythonProject/request_test.py
@@ -7,9 +7,6 @@ response = requests.post(url)
 response_json = response.json()
 
 print(response_json)
+print(type(response_json))
 
-if(response_json == True): print("TRUE")
-else: print("FALSE")
-# for i in response_json:
-#     print(i, "\n")
 
